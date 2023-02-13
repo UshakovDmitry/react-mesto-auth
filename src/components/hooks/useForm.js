8470bfbaae1fd48b48886eea88ebb1ba0f426d01
Dmitry Ushakov <dimau95@yandex.ru>
2023-02-13 18:24:31 +0600

    add hook Formik

--- src/components/hooks/useForm.js
+++ src/components/hooks/useForm.js
@@ -1,49 +1,49 @@
 
-
+import { Formik } from "formik";
 import { useState, useCallback } from "react";
 
 const useForm = () => {
 
-  const [isValues, setIsValues] = useState({});
-  const [isErrors, setIsErrors] = useState({});
-  const [isFormValid, setIsFormValid] = useState(false);
+//   const [isValues, setIsValues] = useState({});
+//   const [isErrors, setIsErrors] = useState({});
+//   const [isFormValid, setIsFormValid] = useState(false);
 
 
 
-  const handleChange = (e) => {
-    setIsValues({
-      ...isValues,
-      [e.target.name]: e.target.value,
-    });
+//   const handleChange = (e) => {
+//     setIsValues({
+//       ...isValues,
+//       [e.target.name]: e.target.value,
+//     });
 
 
-    setIsErrors({
-      ...isErrors,
-      [e.target.name]: e.target.validationMessage,
-    });
-    setIsFormValid(e.target.closest(".form").checkValidity());
-  };
+//     setIsErrors({
+//       ...isErrors,
+//       [e.target.name]: e.target.validationMessage,
+//     });
+//     setIsFormValid(e.target.closest(".form").checkValidity());
+//   };
 
 
 
-  const resetForm = useCallback(
-    (newValues = {}, newErrors = {}, newIsFormValid = false) => {
-      setIsValues(newValues);
-      setIsErrors(newErrors);
-      setIsFormValid(newIsFormValid);
-    },
-    [setIsValues, setIsErrors, setIsFormValid]
-  );
+//   const resetForm = useCallback(
+//     (newValues = {}, newErrors = {}, newIsFormValid = false) => {
+//       setIsValues(newValues);
+//       setIsErrors(newErrors);
+//       setIsFormValid(newIsFormValid);
+//     },
+//     [setIsValues, setIsErrors, setIsFormValid]
+//   );
 
 
   
-  return {
-    isValues,
-    isErrors,
-    handleChange,
-    isFormValid,
-    resetForm,
-  };
-};
+//   return {
+//     isValues,
+//     isErrors,
+//     handleChange,
+//     isFormValid,
+//     resetForm,
+//   };
+// };
 
 export default useForm;
